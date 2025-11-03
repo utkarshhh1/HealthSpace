6e5a02b14bc769054c6206b6f67008a9b463ea0a
Utkarsh <upadhyayu498@gmail.com>
2025-11-04 00:17:41 +0530

    Added and updated user-related files

---
+++ src/main/java/com/healthspace/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.healthspace.controller;
+
+import com.healthspace.entity.User;
+import com.healthspace.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
+    }
+}
---
+++ src/main/java/com/healthspace/service/UserService.java
@@ -0,0 +1,26 @@
+package com.healthspace.service;
+
+import com.healthspace.entity.User;
+import com.healthspace.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
